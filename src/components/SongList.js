66dc00e4c7032a05acc4046bdb42ac00ce5e240a
Craig bauer <craigbauer23@gmail.com>
2020-05-01 14:53:45 -0400

    Connected component to redux store via provider

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+
+    renderSongList() {
+        return this.props.songs.map((song, i) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated item">
+                        <button className="ui button primary">
+                            Selected
+                        </button>
+                    </div>
+                    <div classname="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+
     render() {
         return (
-            <h1>Song List!!!</h1>
+            <div className="ui divided list">
+                {this.renderSongList()}
+            </div>
         );
+        
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return {
+        songs: state.songs
+    }
+    //console.log(state);
+    //return state;
+
+}
+export default connect(mapStateToProps)(SongList);
